2dec1f99b164eac598d86fb3ba807fa333402bab
yunglin <alan@fliptop.com>
2016-02-05 21:43:32 -0800

    Site updated at: 2016-02-06 05:43:32 UTC

--- tweets/data/js/payload_details.js
+++
@@ -1,5 +0,0 @@
-var payload_details =  {
-  "tweets" : 1072,
-  "created_at" : "Wed Apr 17 03:05:26 +0000 2013",
-  "lang" : "en"
-}
--- tweets/data/js/user_details.js
+++
@@ -1,8 +0,0 @@
-var user_details =  {
-  "screen_name" : "yunglinho",
-  "location" : "Taipei",
-  "full_name" : "yunglin ho",
-  "bio" : "Scala, Akka, Cassandra, Distributed, Fault Tolerance, Self-Healing, Scalability, Cloud/Network Management.",
-  "id" : "221733351",
-  "created_at" : "Wed Dec 01 13:22:26 +0000 2010"
-}
